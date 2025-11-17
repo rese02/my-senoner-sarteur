834a846a4c27db92c834b81dd71ea9f104beef7d
Rene Senoner <Senonerrene@gmail.com>
2025-11-17 21:22:57 +0000

    und auch hier in der desktip aslicht nach dem einlgogen sol das logo nci

--- src/app/(customer)/dashboard/layout.tsx
+++ src/app/(customer)/dashboard/layout.tsx
@@ -9,7 +9,7 @@ import { CustomerSidebar } from "./_components/CustomerSidebar";
 function DesktopSidebar() {
   return (
     <aside className="hidden md:flex flex-col w-64 bg-primary border-r border-primary-foreground/10">
-        <div className="p-4 border-b border-primary-foreground/10">
+        <div className="p-4 border-b border-primary-foreground/10 h-20">
             <Logo />
         </div>
         <CustomerSidebar />
