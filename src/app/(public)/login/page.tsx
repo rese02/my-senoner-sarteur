7263c87ddaefd93b3f716080ec0ea9643f3a2e1f
Rene Senoner <Senonerrene@gmail.com>
2025-11-22 09:56:08 +0000

    in mobiler ansicht alspo über dem logo da und unter noch kein konto... d

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -5,27 +5,25 @@ import Link from 'next/link';
 
 export default function LoginPage() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-background md:p-4">
-      <div className="w-full max-w-sm h-full md:h-auto flex flex-col justify-center">
-        <Card className="shadow-none md:shadow-lg border-none rounded-none md:rounded-2xl flex-grow md:flex-grow-0 flex flex-col justify-center">
-            <CardHeader className="text-center items-center pt-12 md:pt-6">
-               <div className="w-2/3 mb-4">
-                 <Logo />
-               </div>
-            <CardTitle className="text-3xl">Anmelden</CardTitle>
-            <CardDescription>Geben Sie Ihre Daten ein, um sich anzumelden.</CardDescription>
-            </CardHeader>
-            <CardContent>
-            <LoginForm />
-            <p className="mt-6 text-center text-sm text-muted-foreground">
-                Noch kein Konto?{' '}
-                <Link href="/register" className="font-semibold text-primary hover:underline">
-                Jetzt registrieren
-                </Link>
-            </p>
-            </CardContent>
-        </Card>
-    </div>
+    <div className="w-full md:min-h-screen md:flex md:items-center md:justify-center md:bg-background md:p-4">
+      <Card className="w-full h-full min-h-screen flex flex-col justify-center shadow-none md:shadow-lg border-none rounded-none md:rounded-2xl md:min-h-0 md:h-auto md:max-w-sm">
+          <CardHeader className="text-center items-center pt-12 md:pt-6">
+              <div className="w-2/3 mb-4">
+                <Logo />
+              </div>
+          <CardTitle className="text-3xl">Anmelden</CardTitle>
+          <CardDescription>Geben Sie Ihre Daten ein, um sich anzumelden.</CardDescription>
+          </CardHeader>
+          <CardContent>
+          <LoginForm />
+          <p className="mt-6 text-center text-sm text-muted-foreground">
+              Noch kein Konto?{' '}
+              <Link href="/register" className="font-semibold text-primary hover:underline">
+              Jetzt registrieren
+              </Link>
+          </p>
+          </CardContent>
+      </Card>
     </div>
   );
 }
