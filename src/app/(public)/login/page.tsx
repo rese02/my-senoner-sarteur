1ac186f34929f4e52d09724eced22c1d7840e8b1
Rene Senoner <Senonerrene@gmail.com>
2025-11-23 19:29:00 +0000

    dieses logo soll 30% kleiner sein (_for element img_)

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
     <div className="w-full md:min-h-screen md:flex md:items-center md:justify-center md:bg-background md:p-4">
       <Card className="w-full h-full min-h-[100dvh] flex flex-col justify-center shadow-none md:shadow-lg border-none rounded-none md:rounded-xl md:min-h-0 md:h-auto md:max-w-sm overflow-hidden">
           <CardHeader className="text-center items-center bg-primary text-primary-foreground pt-12 pb-8">
-              <div className="w-2/3 mb-4">
+              <div className="w-1/2 mb-4">
                 <Logo />
               </div>
           <CardTitle className="text-2xl text-primary-foreground">Anmelden</CardTitle>
--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -8,7 +8,7 @@ export default function RegisterPage() {
     <div className="w-full md:min-h-screen md:flex md:items-center md:justify-center md:bg-background md:py-8 md:px-4">
         <Card className="w-full h-full min-h-[100dvh] flex flex-col justify-center shadow-none md:shadow-lg border-none rounded-none md:rounded-xl md:min-h-0 md:h-auto md:max-w-sm overflow-hidden">
           <CardHeader className="text-center items-center bg-primary text-primary-foreground pt-12 pb-8">
-              <div className="mx-auto w-2/3 mb-6">
+              <div className="mx-auto w-1/2 mb-6">
                 <Logo />
             </div>
             <CardTitle className="text-2xl text-primary-foreground">Konto erstellen</CardTitle>
