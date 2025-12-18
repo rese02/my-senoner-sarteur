9f594cb9e3a68335a01e36ed2333cc01ece8df9c
Rene Senoner <Senonerrene@gmail.com>
2025-12-18 19:46:20 +0000

    bitte dieses background hier alo wenn es im mobine ansicht ist auch dunk

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,5 +1,4 @@
 
-
 import { LoginForm } from './_components/login-form';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import Link from 'next/link';
@@ -10,8 +9,8 @@ export default function LoginPage() {
     <div className="w-full min-h-[100dvh] flex items-center justify-center bg-secondary p-0 md:p-4 relative overflow-hidden">
         <div className="absolute inset-0 z-0 opacity-5 bg-repeat"></div>
         <div className="relative z-10 w-full md:max-w-sm">
-            <Card className="flex flex-col justify-center shadow-2xl border-none rounded-none md:rounded-2xl bg-card text-card-foreground min-h-[100dvh] md:min-h-0 overflow-hidden md:min-h-auto">
-                <CardContent className="flex flex-col items-center justify-center text-center p-8 md:p-10 w-full bg-primary text-primary-foreground">
+            <Card className="flex flex-col justify-center shadow-2xl border-none rounded-none md:rounded-2xl bg-primary text-primary-foreground min-h-[100dvh] md:min-h-0 overflow-hidden md:min-h-auto">
+                <CardContent className="flex flex-col items-center justify-center text-center p-8 md:p-10 w-full">
                     <div className="h-12 mb-6">
                         <Link href="/">
                             <Image src="/logo.png" alt="Senoner Sarteur Logo" width={160} height={40} className="h-full w-auto object-contain" />
